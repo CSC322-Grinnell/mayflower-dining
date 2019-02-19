8825a1938eefcfea87917ccbc26cbfa1426868a8
Ian Young <ian@iangreenleaf.com>
2019-02-19 15:14:02 -0600

    Initial commit

---
+++ app/javascript/delete-me.js
@@ -0,0 +1,5 @@
+// This is an example file that will be imported by Webpacker.
+// If you are using a JavaScript framework such as React, your
+// code will live in this directory and be imported in `app/javascript/packs`.
+
+// console.log('Hello World from Webpacker')
---
+++ app/javascript/packs/application.js
@@ -0,0 +1,11 @@
+/* eslint no-console:0 */
+// All files present in this directory will be automatically compiled by Webpack.
+// Place your actual application logic in a relevant structure within app/javascript
+// and only use these pack files to import that code so it'll be compiled.
+//
+// To reference a pack file, add <%= javascript_pack_tag 'pack-file-name' %> to the appropriate
+// layout file.
+//
+// For more information, see the webpacker gem: https://github.com/rails/webpacker#readme
+
+import 'delete-me'
