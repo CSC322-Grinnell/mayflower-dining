58a686ef877d95b162940f06325db372c1777516
Carlton Segbefia <segbefia@grinnell.edu>
2019-03-04 20:58:22 +0000

    Initial Model Scaffolding

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_19_211347) do
+ActiveRecord::Schema.define(version: 2019_03_04_205135) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -24,6 +24,34 @@ ActiveRecord::Schema.define(version: 2019_02_19_211347) do
     t.index ["author_type", "author_id"], name: "index_active_admin_comments_on_author_type_and_author_id"
     t.index ["namespace"], name: "index_active_admin_comments_on_namespace"
     t.index ["resource_type", "resource_id"], name: "index_active_admin_comments_on_resource_type_and_resource_id"
+  end
+
+  create_table "dishes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string "name"
+    t.string "portionSize?"
+    t.string "mechSoft"
+    t.string "pureed"
+    t.string "diet"
+    t.string "hcPrep"
+    t.string "hcPrepLeftover"
+    t.string "bbPrep"
+    t.string "bbPrpeLeftover"
+    t.string "buckleyPrep"
+    t.string "buckleyPrepLeftover"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "menus", force: :cascade do |t|
+    t.date "day"
+    t.string "typeOfMeal"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
