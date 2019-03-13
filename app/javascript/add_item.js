278f8758631efe745d19f9d32d39b11162711f11
Ivy Xu <xuwenqi@grinnell.edu>
2019-03-13 20:23:44 +0000

    correctly deleting ingredient field

--- app/javascript/add_item.js
+++ app/javascript/add_item.js
@@ -3,19 +3,28 @@ $(document).ready(function() {
     var add_ingredient_button = $(".add_ingredient"); //add_ingredient button
     var save_ingredient_button = $(".save_ingredient"); //save_ingredient button
     
+    var count = 0;
+    
     $(add_ingredient_button).click(function(e) {
+        count++;
         console.log("clicked add_ingredient_button");
         e.preventDefault();
-        var one_ingredient = '<div class="form-group row" id="new_ing">'+
+        
+        `gggg ${count}`
+        
+        var one_ingredient = `<div class="form-group row" id="new_ing_${count}">`+
         '<input type="text" class="col-sm-2 form-control" id="ingredient_name" placeholder="Ingredient"> </input>'+
         '<input type="text" class="col-sm-2 form-control" id="ingredient_amount" placeholder="Amount"> </input>'+
-        '<button class="remove-me">Remove</button></div>';
+        `<button class="remove-me" item=${count}>Remove</button></div>`;
         $(var_wrapper).append(one_ingredient);
         
         $('.remove-me').click(function(e) {
             e.preventDefault();
-            $(this).remove();
-            $('.new_ing').remove();
+           
+            let theThingToDelete = $(this).attr('item');
+            console.log(theThingToDelete);
+            //$(this).remove();
+            $(`#new_ing_${theThingToDelete}`).remove();
         });
     });
     
