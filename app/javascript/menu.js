bec3b499a3ebe1cffed992466616660e1a88a972
CarltonS13 <carltonsegbefia@gmail.com>
2019-04-01 22:38:10 +0000

    fine-tuned menu after merge

--- app/javascript/menu.js
+++ app/javascript/menu.js
@@ -15,7 +15,7 @@
       //var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
       //var str = months[mydate.getMonth()] + ' ' + mydate.getFullYear();
       document.getElementById("headerDate").innerHTML = "Menu for " + mydate;
-      window.location.href = '../pages/menu/' + mydate;
+      window.location.href = '../menu/' + mydate;
       
       // SEND A GET REQUEST TO YOUR HTML WITH THE DATE
       // THIS WILL RETURN A SAME HTML PAGE BUT WITH NEW DISHES ADDED ACCORDING TO DATE
@@ -29,14 +29,14 @@
       var tom = new Date();
       tom.setDate(d.getDate()+1);
       document.getElementById("datePicker").value = formatDate(tom);
-      window.location.href = '../pages/menu/' + document.getElementById("datePicker").value;
+      window.location.href = '../menu/' + document.getElementById("datePicker").value;
     }
     
     function changeToToday () {
       document.getElementById("headerDate").innerHTML = "Menu for Today";
       var d = new Date();
       document.getElementById("datePicker").value = formatDate(d);
-      window.location.href = '../pages/menu/' + document.getElementById("datePicker").value;
+      window.location.href = '../menu/' + document.getElementById("datePicker").value;
     }
     
     window.addEventListener('DOMContentLoaded', (event) => {
