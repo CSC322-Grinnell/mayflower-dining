1ab1b0cebcf9efaaaf0876ce0ca1f3a3e91efd26
CarltonS13 <carltonsegbefia@gmail.com>
2019-04-03 20:35:38 +0000

    changing menu date header

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -10,19 +10,19 @@ class MenuController < ApplicationController
         date = params[:date]
         dishes = Menu.get_dishes_by_date(date)
         
-        today = Time.now.strftime("%Y-%m-%d")
-        tommorrow = Time.now + 24*60*60
-        tommorrow = tommorrow.strftime("%Y-%m-%d")
+        # today = Time.now.strftime("%Y-%m-%d")
+        # tommorrow = Time.now + 24*60*60
+        # tommorrow = tommorrow.strftime("%Y-%m-%d")
         
-        if  today == date
-            date = "Today"
-        elsif tommorrow == date
-            date = "Tomorrow"
-        else
-            date = Date.parse(date).strftime('%A') + ", " + date.to_s
-        end
+        # if  today == date
+        #     date = "Today"
+        # elsif tommorrow == date
+        #     date = "Tomorrow"
+        # else
+        #     date = Date.parse(date).strftime('%A') + ", " + date.to_s
+        # end
         
-        @menu_date = date
+        # @menu_date = date
         @dishes = dishes.to_a
     end
 end
