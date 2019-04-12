da60a6df8e3e45355bf6d9933a753db8909d836d
Yesheng Chen <chenyesh@grinnell.edu>
2019-04-12 19:35:13 +0000

    html for checklist table done

--- app/controllers/prep_page_controller.rb
+++ app/controllers/prep_page_controller.rb
@@ -1,4 +1,8 @@
 class PrepPageController < ApplicationController
   def prep_page
+    date = Time.now.strftime("%d/%m/%Y")
+    if Menu.get_ingredients_by_date(date) != nil
+      @ingriedent_array= Menu.get_ingredients_by_date(date)
+    end
   end
 end
