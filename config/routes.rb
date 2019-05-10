a0b85ed10f04e7f20db1c92a68c53c7b976db0cb
CarltonS13 <carltonsegbefia@gmail.com>
2019-05-10 14:56:51 +0000

    merge fixes

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,10 @@ Rails.application.routes.draw do
   get 'menu/:date', to: 'menu#new_date'
   get 'temp', to: 'temporary_menu#menu'
   get 'temp/:date', to: 'temporary_menu#new_date'
+  get 'permanent_edit', to: 'permanent_edit#permanent_edit'
+  get 'permanent_edit/:week/:day', to: 'permanent_edit#view_dishes'
+  get 'prep/prep_page', to: 'prep#preppage'
+  get 'prep/prep_page/:id', to: 'prep#checked'
 
   root to: "pages#home"
   ActiveAdmin.routes(self)
