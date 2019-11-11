7a5506cf0f8d7d57c59629eefeb08f94367956a4
wyk9787 <wangyika@grinnell.edu>
2019-11-11 11:38:55 -0600

    Add rest of the hard-coded data to the seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,25 +2,29 @@
 User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password', admin: true) if Rails.env.development?
 
 # Import the csv table and remove all blank rows
-table = CSV.parse(File.read("db/menus/cycle1.csv"), headers: true, skip_blanks: true) \
-  .delete_if { |row| row.to_hash.values.all?(&:blank?) }
+Dir.foreach('db/menus/') do |filename|
+  next if filename == '.' or filename == '..'
 
-day = 0
-row = 0
-
-while row < table.length
-  dish_ids = Array.new
-
-  # Each day ends on "CYCLE: xxx"
-  while !table[row][0].include? "CYCLE" 
-    d = Dish.create({name: table[row][0]})
-    # Add the id for the newly created dish
-    dish_ids.push(d.id)
-    row += 1
+  table = CSV.parse(File.read(filename), headers: true, skip_blanks: true) \
+    .delete_if { |row| row.to_hash.values.all?(&:blank?) }
+  
+  day = 0
+  row = 0
+  
+  while row < table.length
+    dish_ids = Array.new
+  
+    # Each day ends on "CYCLE: xxx"
+    while !table[row][0].include? "CYCLE" 
+      d = Dish.create({name: table[row][0]})
+      # Add the id for the newly created dish
+      dish_ids.push(d.id)
+      row += 1
+    end
+  
+    Menu.create({day: day, type_of_meal: "Dinner", dish_ids: dish_ids})
+    day += 1
+    # Skip two rows: "CYCLE: xxx" and another header
+    row += 2
   end
-
-  Menu.create({day: day, type_of_meal: "Dinner", dish_ids: dish_ids})
-  day += 1
-  # Skip two rows: "CYCLE: xxx" and another header
-  row += 2
 end
