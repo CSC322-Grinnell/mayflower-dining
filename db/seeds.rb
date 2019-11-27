157c820852dbff2fca4b72a822255a916df7edba
wyk9787 <wangyika@grinnell.edu>
2019-11-26 22:32:42 -0600

    Fix hardcoded data and seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,49 @@
 # Create admin user
 User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password', admin: true) if Rails.env.development?
 
-# Import the csv table and remove all blank rows
-table = CSV.parse(File.read("db/menus/cycle1.csv"), headers: true, skip_blanks: true) \
-  .delete_if { |row| row.to_hash.values.all?(&:blank?) }
+kMenuDir = 'db/menus/'
 
 day = 1
-row = 0
 
-while row < table.length
-  # Each day ends on "CYCLE: xxx"
-  while !table[row][0].include? "CYCLE"
-    type = "Dinner" # default type
-    name = table[row][0]
-    if name.start_with?("*")
-      type = "Supper"
-      name = name[1..-1]
-    elsif name.start_with?("**")
-      type = "Dinner"
-      name = name[2..-1]
+Dir.foreach(kMenuDir) do |filename|
+  next if filename == '.' or filename == '..'
+
+  table = CSV.parse(File.read(kMenuDir + filename), headers: true, skip_blanks: true) \
+    .delete_if { |row| row.to_hash.values.all?(&:blank?) }
+
+  row = 0
+  while row < table.length
+    # Each day ends on "CYCLE: xxx"
+    while !table[row][0].include? "CYCLE"
+      type = "Dinner" # default type
+      name = table[row][0]
+      if name.start_with?("*")
+        type = "Supper"
+      elsif name.start_with?("**")
+        type = "Dinner"
+      end
+  
+      # Replace any leading non-word character with empty space
+      name = name.sub!(/^\W*/, '')
+  
+      if !Dish.exists?(name: name)
+        dish = Dish.create({name: name})
+      else
+        dish = Dish.find_by(name: name)
+      end
+      Menu.add_dish_to_cycle(day, type, dish)
+  
+      # Add the id for the newly created dish
+      row += 1
+
     end
-    dish = Dish.create({name: name})
-    Menu.add_dish_to_cycle(day, type, dish)
+  
+    day += 1
+    # Skip two rows: "CYCLE: xxx" and another header
+    row += 2
 
-    # Add the id for the newly created dish
-    row += 1
   end
 
-  day += 1
-  # Skip two rows: "CYCLE: xxx" and another header
-  row += 2
+
 end
+
