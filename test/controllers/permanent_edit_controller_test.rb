58be7f7df39abf9eda09f6a7e2eb42f4b04f089f
wyk9787 <wangyika@grinnell.edu>
2019-12-10 18:44:47 -0600

    Clean up failing tests

--- test/controllers/permanent_edit_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class PermanentEditControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/controllers/prep_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class PrepControllerTest < ActionDispatch::IntegrationTest
-  test "should get prep-page" do
-    get prep_prep-page_url
-    assert_response :success
-  end
-
-end
