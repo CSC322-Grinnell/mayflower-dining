e9d7c2f4044078748a4def0c39ec2720af058363
arthurhero <chenziwe@grinnell.edu>
2020-02-29 20:01:30 -0600

    update loc test fixture

--- test/models/location_test.rb
+++ test/models/location_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class LocationTest < ActiveSupport::TestCase
     test 'get locations' do
         locs=Location.all
+        assert locs.length()==3
         locs.each do |loc|
             assert_instance_of(String,loc.name)
             assert_instance_of(Integer,loc.population)
