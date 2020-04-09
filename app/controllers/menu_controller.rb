47ef7b19c082a0a58b3a05ca4c2d18a0d606e451
GouwarPower <jgouwar@gmail.com>
2020-04-08 23:20:05 -0400

    Fixes issue with dates, needed to catch an alternate date type.

--- app/controllers/menu_controller.rb
+++ app/controllers/menu_controller.rb
@@ -56,24 +56,31 @@ class MenuController < ApplicationController
          else
              raise "Unknown action"
          end
- 
+
      rescue => e
          flash[:error] = e
      end
- 
+
      redirect_to '?date=' + date
     end
- 
+
     private
     # Local helper methods
 
     # Takes a day in dd/mm/yyyy format and converts it to the number of days
     # since 20/01/1999 mod 49, since dishes are stored from days 0-48
     def convert_date_to_day(date)
-      parse_string = "%d/%m/%Y"
-      start_date = Date.strptime("22/12/2011", parse_string)
-      curr_date = Date.strptime(date, parse_string)
-      return (curr_date - start_date).to_i % 49
+      begin
+        parse_string = "%d/%m/%Y"
+        start_date = Date.strptime("22/12/2011", parse_string)
+        curr_date = Date.strptime(date, parse_string)
+        return (curr_date - start_date).to_i % 49
+      rescue
+        parse_string = "%Y-%m-%d"
+        start_date = DateTime.strptime("2011-12-22", parse_string)
+        curr_date = DateTime.strptime(date, parse_string)
+        return (curr_date - start_date).to_i % 49
+      end
     end
 
     #finds the dishes and their recipies for a given meal_type and given day
