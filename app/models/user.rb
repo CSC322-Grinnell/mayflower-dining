0e1bc47d9b841ec08b922d13fac581fdcb372a96
GouwarPower <jgouwar@gmail.com>
2020-04-20 16:56:40 -0400

    Adds validations for password length, password confirmation, and unique email to User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
+
+  validates :password, length: { minimum: 3 }, if: -> { new_record? || crypted_password_changed? } # or you can use changes[:crypted_password] instead of crypted_password_changed? if you wish
+  # the confirmation setting checks whether the password and password_confirmation match
+  validates :password, confirmation: true, if: -> { new_record? || crypted_password_changed? }
+  validates :password_confirmation, presence: true, if: -> { new_record? || crypted_password_changed? }
+
+  validates :email, uniqueness: true
+  
 end
