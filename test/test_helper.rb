f7d5b6a40ec13662c34f754d00d3e7d870f26562
niehusst <liamniehusstaab@gmail.com>
2020-04-19 23:38:54 -0600

    basic (but failing) tests for menu controller

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 require "#{Rails.root}/db/seeds.rb"
+require 'Date'
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
