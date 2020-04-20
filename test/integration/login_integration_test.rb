5bcb42b00c1e0769680e88121bc6a0b30868de58
niehusst <liamniehusstaab@gmail.com>
2020-04-20 17:20:21 -0600

    removed debugging stuff

--- test/integration/login_integration_test.rb
+++ test/integration/login_integration_test.rb
@@ -3,9 +3,6 @@ require 'test_helper'
 class LoginIntegrationTest < Capybara::Rails::TestCase
 
     setup do
-        u = User.all()[0]
-        puts "----- #{User.all().size} the email is ::::: #{u.email}"
-        puts u.methods.sort
         @email = 'admin@example.com'
         @pass = 'password'
     end
@@ -21,7 +18,6 @@ class LoginIntegrationTest < Capybara::Rails::TestCase
         find('#user_email').fill_in with: @email
         find('#user_password').fill_in with: @pass
         find('input[value="Log in"]').click
-#puts page.html
         assert page.has_text?('Signed in successfully.'), 'Successful sign in flash not present'
         
         #assert redirect to admin panel
@@ -30,7 +26,6 @@ class LoginIntegrationTest < Capybara::Rails::TestCase
         #assert logout takes back to original view
         visit '/'
         assert page.has_text?('Add Dish to Menu', count: 1), 'Add dish button not present for logged in admin user'
-#puts page.html
         find('a[href="/users/sign_out"]').click
         assert page.has_text?('Signed out successfully.'), 'Sign out flash not present'
         assert page.has_text?('Add Dish to Menu', count: 0), 'Add dish button present after logout'
