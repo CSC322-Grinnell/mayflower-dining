e1ff744f7ea79949b67247470b275b515d3a6764
niehusst <liamniehusstaab@gmail.com>
2020-04-19 20:45:15 -0600

    removed dead routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
 #--------------------------------------- menu
-
-
   # api to edit the menu (add dish, delete dish)
   get 'menu/edit/(:date/:acts/:dish/:type/:permanent)', to: 'menu#edit'
   # show menu(w/ buttons to delete/add dishes). date = optional parameter. 
@@ -20,18 +18,8 @@ Rails.application.routes.draw do
 
 # api to get all dishes
   get 'dishes/all', to: 'dishes#all'
-#--------------------------------------- ingredients 
+#---------------------------------------
 
-# api to edit the ingredient name/total_amount
- post 'ingredients/edit/', to: 'ingredients#edit'
-
-# api to make new ingredient for a dish
- post 'ingredients/new', to: 'ingredients#new'
-
-# api to delete the ingredient and all its occurences
- delete 'ingredients/:name', to: 'ingredients#delete'
-
-  
   root to: "menu#menu"
   ActiveAdmin.routes(self)
   devise_for :users
