60d9c977f5e629270c39ac0ae9e76e54e1d7c05f
GouwarPower <jgouwar@gmail.com>
2020-04-22 10:57:45 -0400

    Creates basic validity tests for users.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,51 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @staff = User.new(email: "email@email.com", password:"password",
+                      password_confirmation: "password", name:"Staff",
+                      admin: false)
+    @admin = User.new(email: "email2@email.com", password:"password",
+                      password_confirmation: "password", name:"Staff",
+                      admin: true)
+  end
+
+  test "setup staff valid" do
+    assert @staff.valid?
+  end
+
+  test "setup admin valid" do
+    assert @admin.valid?
+  end
+
+  test "password should be at least 3 characters" do
+    @staff.password = "ab"
+    @staff.password_confirmation = "ab"
+    assert_not @staff.valid?
+  end
+
+  test "need password confirmation" do
+    @staff.password_confirmation = ""
+    assert_not @staff.valid?
+  end
+
+  test "password confirmation must match" do
+    @admin.password_confirmation = "password1"
+    assert_not @admin.valid?
+  end
+
+  test "admin setter works with bool" do
+    assert_not @staff.admin
+    assert @admin.admin
+  end
+
+  test "admin setter works with string 'Staff' " do
+    @staff.admin = "Staff"
+    assert_not @staff.admin
+  end
+
+  test "admin setter works with string 'Admin' " do
+    @admin.admin = "Admin"
+    assert @admin.admin
+  end
 end
