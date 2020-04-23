2790f3d9d01c45d04744a538f156be1a023bc120
GouwarPower <jgouwar@gmail.com>
2020-04-23 14:11:00 -0400

    Changes require 'Date' to require 'date' to fix functionality on linux.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 require "#{Rails.root}/db/seeds.rb"
-require 'Date'
+require 'date'
 require "minitest/rails/capybara"
 
 class ActiveSupport::TestCase
